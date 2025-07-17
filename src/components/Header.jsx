c643c7c2c9495c661812907a7e25a8bffb82645b
simone-scotto <simone.scottotkd@gmail.com>
2025-07-17 17:17:06 +0200

    first leyout

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header () {
+    return (
+        <header>
+            <h1>Learn Web development</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+export default function Main () {
+    return (
+        <div className="container">
+            <div className="btn-section">
+                <button type="button" className="btn btn-primary">HTML</button>
+                <button type="button" className="btn btn-primary">CSS</button>
+                <button type="button" className="btn btn-primary">JavaScript</button>
+                <button type="button" className="btn btn-primary">Node.js</button>
+                <button type="button" className="btn btn-primary">Express</button>
+                <button type="button" className="btn btn-primary">ReactJS</button>
+            </div>
+            <div className="card">
+                <h2>Nessun Linguaggio Selezionato</h2>
+            </div>
+            
+        </div>
+    )
+}
