14fffd7a7aa830983d0801b9198a2dfe3a81e6d8
simone-scotto <simone.scottotkd@gmail.com>
2025-07-21 09:40:41 +0200

    added color

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,54 @@
+
+import webLanguages from "../db/webdev"
+
+
+import { useState } from "react"
+
+
 export default function Main () {
+
+const [selectedLang, setSelectedLang] = useState(null)
+    
+
+    function handleClick (lang){
+        console.log(lang);
+        
+        setSelectedLang(lang)        
+    }
     return (
         <div className="container">
+
             <div className="btn-section">
-                <button type="button" className="btn btn-primary">HTML</button>
-                <button type="button" className="btn btn-primary">CSS</button>
-                <button type="button" className="btn btn-primary">JavaScript</button>
-                <button type="button" className="btn btn-primary">Node.js</button>
-                <button type="button" className="btn btn-primary">Express</button>
-                <button type="button" className="btn btn-primary">ReactJS</button>
+                {webLanguages.map(({id, title, description}) => {
+                    return (
+                        
+                        <button onClick={()=>handleClick({id, title, description})} key={id} type="button" className={`btn ${selectedLang?.id === id ? "btn-warning" : "btn-primary"}`}>{title}</button>
+                        
+                        
+                        
+                    )
+                })}
+
             </div>
-            <div className="card">
-                <h2>Nessun Linguaggio Selezionato</h2>
-            </div>
-            
-        </div>
+
+            <div className="card-section">
+                {selectedLang ?  (
+                    <>
+                    <div className="card">
+                        <h3>{selectedLang.title}</h3>
+                        <p>{selectedLang.description}</p>
+                    </div>
+                    </>
+                    )  : (
+                    <div className="card">
+                        <h3>nessun linguaggio solezionato</h3>
+                        <p>selezionare un linguaggio</p>
+                    </div>
+
+                )
+                }
+
+            </div>    
+        </div>    
     )
 }
---
+++ src/db/webdev.js
@@ -0,0 +1,40 @@
+const webLanguages = [
+  {
+    id: 1,
+    title: "HTML",
+    description:
+      "Lorem HTML ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vero illum dolorem quia quibusdam ipsum dolore nam cupiditate quis rem consectetur, fugit itaque, magni architecto velit iure quasi, enim numquam!",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    description:
+      "Lorem CSS ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vero illum dolorem quia quibusdam ipsum dolore nam cupiditate quis rem consectetur, fugit itaque, magni architecto velit iure quasi, enim numquam!",
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    description:
+      "Lorem JAVASCRIPT ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vero illum dolorem quia quibusdam ipsum dolore nam cupiditate quis rem consectetur, fugit itaque, magni architecto velit iure quasi, enim numquam!",
+  },
+  {
+    id: 4,
+    title: "Node.js",
+    description:
+      "Lorem NODE ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vero illum dolorem quia quibusdam ipsum dolore nam cupiditate quis rem consectetur, fugit itaque, magni architecto velit iure quasi, enim numquam!",
+  },
+  {
+    id: 5,
+    title: "Express",
+    description:
+      "Lorem EXPRESS ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vero illum dolorem quia quibusdam ipsum dolore nam cupiditate quis rem consectetur, fugit itaque, magni architecto velit iure quasi, enim numquam!",
+  },
+  {
+    id: 6,
+    title: "ReactJS",
+    description:
+      "Lorem REACT ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati vero illum dolorem quia quibusdam ipsum dolore nam cupiditate quis rem consectetur, fugit itaque, magni architecto velit iure quasi, enim numquam!",
+  },
+];
+
+export default webLanguages;
